e663dc8d6e01be54834d65f6a272e2002654520b
alphacpc <alphacpc@gmail.com>
2022-06-04 16:42:39 +0000

    Ajout des fichier static et un peu de contenu

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
+
+
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    return render_template('pages/home.html')
+
+
+
+
+@app.route("/connexion/")
+def login():
+    return render_template('pages/login.html')
+
+
+
+
+@app.route("/inscription/")
+def register():
+    return render_template('pages/register.html')
+
+
+
+
+
+# @app.errorhandler(404)
+# def not_found(error):
+#     return render_template('pages/not_found.html'),404
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True,port=5000)
